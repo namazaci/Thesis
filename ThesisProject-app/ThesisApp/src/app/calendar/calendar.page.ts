d49c124ae2ab67459119c73e9783bfabcef28001
namazaci <34474282+namazaci@users.noreply.github.com>
2019-04-16 17:36:17 +0200

    Finished coding the calendar

--- ThesisProject-app/ThesisApp/src/app/calendar/calendar.page.ts
+++ ThesisProject-app/ThesisApp/src/app/calendar/calendar.page.ts
@@ -68,4 +68,53 @@ export class CalendarPage implements OnInit {
     this.myCal.loadEvents();
     this.resetEvent();
   }
+
+  // Chnage current month/week/day
+  next() {
+    var swiper = document.querySelector('.swiper-container')['swiper'];
+    swiper.slideNext();
+  }
+
+  back() {
+    var swiper = document.querySelector('swiper-container')['swiper'];
+    swiper.slidePrev();
+  }
+
+  // Change between month/week/day
+  changeMode(mode) {
+    this.calendar.mode = mode;
+  }
+
+  // Focus today
+  today() {
+    this.calendar.currentDate = new Date();
+  }
+
+  // Selected date reange and hence title changed
+  onViewTitleChanged(title) {
+    this.viewTitle = title;
+  }
+
+  // Calendar event was clicked 
+  async onEventSelected(event) {
+    // Use Angular date pipe for conversion
+    let start = formatDate(event.startTime, 'medium', this.locale);
+    let end = formatDate(event.endTime, 'medium', this.locale);
+
+    const alert = await this.alertCtrl.create({
+      header: event.title,
+      subHeader: event.desc,
+      message: 'From: ' + start + '<br><br>To: ' + end,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  // Time slot was clicked
+  onTimeSelected(ev){
+    let selected = new Date(ev.selctedTime);
+    this.event.startTime = selected.toISOString();
+    selected.setHours(selected.getHours() + 1);
+    this.event.endTime = (selected.toISOString());
+  }
 }
